be5b1c7c2641e54d0b65d5becded7386a95fe633
Wiley Carlin <wileync3@gmail.com>
2024-09-06 09:18:48 -0600

    I counted the lines in a file with python

--- line_count.py
+++ line_count.py
@@ -1,4 +1,15 @@
 def line_count():
-  """
-  Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count.
-  """
+  count = 0
+  #start the count at 0
+  file = open('file.txt', 'r')
+  #open the file
+  lines = file.readlines()
+  #get the program to read the lines in the file
+  for line in lines:
+    #start a for loop
+    count = count + 1
+    #get the program to add 1 to count, everytime it reads a line
+  file.close()
+  #close the file to save and not screw up the data
+  return(count)
+  #get the program to return you the number of lines it counted in the file
